66da4192a9ad846b9bf01657216a99f49fdb2bfd
João Vitor Soares <jvsoares082004@gmail.com>
2025-04-26 00:50:36 -0300

    adding git ignore e docker file

--- server/api/Models/Vehicles.js
+++ server/api/Models/Vehicles.js
@@ -1,9 +1,19 @@
 import { Sequelize, DataTypes } from "sequelize";
 
-const db = new Sequelize('testing', 'root', 'ANSKk08aPEDbFjDO', {
-    host: 'localhost', 
-    port: 3307,
-    dialect: 'mysql'
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 3307;
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'ANSKk08aPEDbFjDO';
+const DB_NAME = process.env.DB_NAME || 'testing';
+
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST, 
+    port: DB_PORT,
+    dialect: 'mysql',
+    retry: {
+        max: 5,
+        timeout: 60000
+    }
 });
 
 const vehicle = db.define("Vehicles", {
